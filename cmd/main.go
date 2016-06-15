286ea02431f6467bea12384122194999961441e1
Mark A. Richman <mark@markrichman.com>
2016-06-15 14:58:18 -0400

    Initial revision

---
+++ cmd/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"sync"
+
+	"github.com/mrichman/godnsbl"
+)
+
+func main() {
+
+	if len(os.Args) != 2 {
+		fmt.Println("Please supply a domain name or IP address.")
+		os.Exit(1)
+	}
+
+	ip := os.Args[1]
+
+	wg := &sync.WaitGroup{}
+	results := make([]godnsbl.Result, len(godnsbl.Blacklists))
+	for i, source := range godnsbl.Blacklists {
+		wg.Add(1)
+		go func(i int, source string) {
+			defer wg.Done()
+			rbl := godnsbl.Lookup(source, ip)
+			results[i] = rbl.Results[0]
+		}(i, source)
+	}
+
+	wg.Wait()
+
+	enc := json.NewEncoder(os.Stdout)
+	if err := enc.Encode(&results); err != nil {
+		log.Println(err)
+	}
+}
---
+++ godnsbl_test.go
@@ -0,0 +1,40 @@
+package godnsbl
+
+import (
+	"fmt"
+	"net"
+	"testing"
+)
+
+// Parallel for loop: https://play.golang.org/p/MVFwbmxbou
+// http://www.golangpatterns.info/concurrency/parallel-for-loop
+
+func TestReverseIP(t *testing.T) {
+	fmt.Println("TestReverseIP")
+	t.Parallel()
+	ip := net.IP{192, 168, 1, 1}
+
+	r := Reverse(ip)
+
+	if r != "1.1.168.192" {
+		t.Errorf("Expected ip to equal 1.1.168.192, actual %v", r)
+	}
+}
+
+func TestLookupParams(t *testing.T) {
+	fmt.Println("TestLookupParams")
+	t.Parallel()
+
+	for i := range Blacklists {
+		res := Lookup(Blacklists[i], "www.example.com")
+		fmt.Println(res.Results)
+
+		if res.List != "b.barracudacentral.org" {
+			t.Errorf("Expected b.barracudacentral.org, actual %v", res.List)
+		}
+
+		if res.Host != "www.markrichman.com" {
+			t.Errorf("Expected www.markrichman.com, actual %v", res.Host)
+		}
+	}
+}
